62e7fb24395964ba480071cc49d23320b30462f4
adhamaa <adham_92@live.com>
2021-04-10 13:52:45 +0800

    fix search bar

--- pages/index.js
+++ pages/index.js
@@ -36,12 +36,9 @@ export default function Home() {
           width={300}
           height={100}
         />
-        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md">
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl">
           <SearchIcon className="h-5 mr-3 text-gray-500" />
-          <input
-            type="text"
-            className="flex-grow focus:outline-none rounded-full border border-gray-200"
-          />
+          <input type="text" className="flex-grow focus:outline-none" />
           <MicrophoneIcon className="h-5" />
         </div>
       </form>
